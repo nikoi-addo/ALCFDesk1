e7246fd14df77ce08c334ff942d3c7faae01dfbc
Mingleemma <mingleemma09@gmail.com>
2019-07-16 18:35:14 +0000

    db

--- alcfdesk.sql
+++ alcfdesk.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 15, 2019 at 04:14 AM
+-- Generation Time: Jul 16, 2019 at 06:34 PM
 -- Server version: 5.7.26-0ubuntu0.16.04.1
 -- PHP Version: 7.0.33-0ubuntu0.16.04.5
 
@@ -19,6 +19,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `alcfdesk`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(8) NOT NULL,
+  `username` varchar(12) NOT NULL,
+  `password` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'Mingleemma', 0x3862656261386131616434343737363466633761303162366338383135313062);
 
 -- --------------------------------------------------------
 
@@ -51,15 +70,21 @@ CREATE TABLE `eventregistration` (
 --
 
 INSERT INTO `eventregistration` (`id`, `fname`, `lname`, `email`, `phone`, `location`, `dob`, `dattend1`, `attend1`, `dattend2`, `attend2`, `dattend3`, `attend3`, `source`, `expectations`, `usercode`, `registrar`) VALUES
-(1, 'a', 'b', 'a@g.com', '0263192377', 'a', '2019-07-11', 'no', 'N/A', 'yes', 'N/A', 'yes', 'N/A', 'cat1', '', 'ALC5d289e848b865', 'Rudimentary'),
-(2, 'My', 'Name', 'is@not.necessary', '02774839203', 'But', '2019-07-29', 'yes', 'yes', 'no', 'N/A', 'yes', 'N/A', 'cat1', 'Great expectations, for the power of God to be revealed.', 'ALC5d29032b479ed', 'Rudimentary'),
-(3, 'Frank', 'Olsen', 'frolansekn@mail.com', '0303193299', 'Accra', '2019-07-09', 'no', 'yes', 'yes', 'N/A', 'no', 'N/A', 'cat2', 'Great ones', 'ALC5d290a4769381', 'Rudimentary'),
+(1, 'a', 'b', 'a@g.com', '0263192377', 'a', '2019-07-11', 'no', 'yes', 'yes', 'N/A', 'yes', 'N/A', 'cat1', '', 'ALC5d289e848b865', 'Rudimentary'),
+(2, 'My', 'Name', 'is@not.necessary', '02774839203', 'But', '2019-07-29', 'yes', 'N/A', 'no', 'N/A', 'yes', 'N/A', 'cat1', 'Great expectations, for the power of God to be revealed.', 'ALC5d29032b479ed', 'Rudimentary'),
+(3, 'Frank', 'Olsen', 'frolansekn@mail.com', '0303193299', 'Accra', '2019-07-09', 'no', 'N/A', 'yes', 'N/A', 'no', 'N/A', 'cat2', 'Great ones', 'ALC5d290a4769381', 'Rudimentary'),
 (4, 'Lara', 'Vel', 'vellara@mailto.io', '124', 'Tema', '2019-07-09', 'no', 'N/A', 'yes', 'N/A', 'no', 'N/A', 'cat2', 'HOpe', 'ALC5d290b95a00d0', 'Rudimentary'),
-(6, 'Load', 'Balance', 'load@gmail.com', '02774839203', 'Tarkwa', '', 'no', 'N/A', 'no', 'N/A', 'yes', 'N/A', 'cat0', '', 'ALC5d291036b82a6', 'Rudimentary');
+(6, 'Load', 'Balance', 'load@gmail.com', '02774839203', 'Tarkwa', '', 'no', 'yes', 'no', 'N/A', 'yes', 'N/A', 'cat0', '', 'ALC5d291036b82a6', 'Rudimentary');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `eventregistration`
@@ -72,6 +97,11 @@ ALTER TABLE `eventregistration`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `eventregistration`
 --
